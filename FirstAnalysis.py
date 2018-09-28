b502e21792aecccc871f6ba1b4beaf1f19fce68a
Drokonor <terminusdrok@gmail.com>
2018-09-28 20:07:02 +0300

    First Analysis

---
+++ FirstAnalysis.py
@@ -0,0 +1,19 @@
+import re
+import os 
+files = os.listdir()
+texts = filter(lambda x: x.endswith('.txt'), files)
+def FirstAnalysis(str,ALL):
+ file = open(str,'r')
+ R = file.read()
+ file.close()
+ n = R.find('Мнение судьи Конст')
+ R = R[0:n]
+
+ pattern = re.compile(r'[Оо].\s\d+\s[яфмаисонд]\w+\s\d+\sгода\s№\s\d[^ ),]+')
+ result = pattern.findall(R)
+ #print (result)
+ ALL[str] = result
+ALL = {} 
+for text in texts:
+ FirstAnalysis(text,ALL) 
+print (ALL)
