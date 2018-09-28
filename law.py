76860ae8a56ff2bad286e0bd95c73ada5fe2a55b
Влад Купряков <kupryakov@MacBook-Air-Vlad.local>
2018-09-28 15:45:07 +0300

    trying to fix unicode problems

--- law.py
+++ law.py
@@ -1,13 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fileencoding=utf-8
 import urllib
 import re
 html = urllib.urlopen("http://www.ksrf.ru/ru/Decision/Pages/default.aspx")
 htmlForParse = html.read()
-print htmlForParse
+htmlForParseU =  unicode(htmlForParse, "utf-8")
 pdfLinks = re.findall(r'(http://doc.ksrf.ru/decision/KSRFDecision\d{6}\b\.pdf|http://doc.ksrf.ru/decision/KSRFDecision\d{5}\b\.pdf)', htmlForParse)
 date = re.findall(r'\d\d\.\d\d\.\d{4}', htmlForParse)
-uid = re.findall(r'_blank">................', htmlForParse)
+uid = re.findall(r'(\d{4}-О/\d{4})|(ПР-\d{1}/\d{4})', htmlForParse)
 print uid
-# \d{2}-/2018
+# |(\d{3}-О/\d{4})|(\d{2}-О/\d{4})|(\d{1}-О/\d{4})
+# |(\d{3}-ПРП/\d{4})|(\d{2}-ПРП/\d{4})|(\d{1}-ПРП/\d{4})|(ПР-\d{1}/\d{4})|(\d{3}-П/\d{4})|(\d{2}-П/\d{4})|(\d{1}-П/\d{4})
+# |(\d{3}-Р/\d{4})|(\d{2}-Р/\d{4})|(\d{1}-Р/\d{4})|(\d{4}-О-Р/\d{4})|(\d{3}-О-Р/\d{4})|(\d{2}-О-Р/\d{4})|(\d{1}-О-Р/\d{4})
 print pdfLinks
 print date
 #format {'uid': {'date': 'date string', 'url': 'link to pdf file'}}
