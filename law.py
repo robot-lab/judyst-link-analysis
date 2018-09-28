29efe7a9c67ae4f89a090215fddaf733ba03aa35
Влад Купряков <kupryakov@MacBook-Air-Vlad.local>
2018-09-28 17:13:43 +0300

    test

--- law.py
+++ law.py
@@ -9,12 +9,12 @@ htmlForParseU =  unicode(htmlForParse, "utf-8")
 pdfLinks = re.findall(r'(http://doc.ksrf.ru/decision/KSRFDecision\d{6}\b\.pdf|http://doc.ksrf.ru/decision/KSRFDecision\d{5}\b\.pdf)', htmlForParse)
 date = re.findall(r'\d\d\.\d\d\.\d{4}', htmlForParse)
 uid = re.findall(r'(\d{4}-О/\d{4})|(ПР-\d{1}/\d{4})', htmlForParse)
-print uid
+print (uid)
 # |(\d{3}-О/\d{4})|(\d{2}-О/\d{4})|(\d{1}-О/\d{4})
 # |(\d{3}-ПРП/\d{4})|(\d{2}-ПРП/\d{4})|(\d{1}-ПРП/\d{4})|(ПР-\d{1}/\d{4})|(\d{3}-П/\d{4})|(\d{2}-П/\d{4})|(\d{1}-П/\d{4})
 # |(\d{3}-Р/\d{4})|(\d{2}-Р/\d{4})|(\d{1}-Р/\d{4})|(\d{4}-О-Р/\d{4})|(\d{3}-О-Р/\d{4})|(\d{2}-О-Р/\d{4})|(\d{1}-О-Р/\d{4})
-print pdfLinks
-print date
+print (pdfLinks)
+print (date)
 #format {'uid': {'date': 'date string', 'url': 'link to pdf file'}}
 # logo = urllib.urlopen("http://doc.ksrf.ru/decision/KSRFDecision343971.pdf").read()
 # f = open("l.pdf", "wb")
