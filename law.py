6d70cb001b16dd4cb6badc215732759fbf891db5
Влад Купряков <kupryakov@44.14.20.172.in-addr.arpa>
2018-09-28 11:38:23 +0300

    add regex for date and pdfLinks

--- law.py
+++ law.py
@@ -1,7 +1,17 @@
 import urllib
-f = urllib.urlopen("http://www.ksrf.ru/ru/Decision/Pages/default.aspx")
-print f.read()
-logo = urllib.urlopen("http://doc.ksrf.ru/decision/KSRFDecision343971.pdf").read()
-f = open("l.pdf", "wb")
-f.write(logo)
-f.close()
+import re
+html = urllib.urlopen("http://www.ksrf.ru/ru/Decision/Pages/default.aspx")
+htmlForParse = html.read()
+print htmlForParse
+pdfLinks = re.findall(r'(http://doc.ksrf.ru/decision/KSRFDecision\d{6}\b\.pdf|http://doc.ksrf.ru/decision/KSRFDecision\d{5}\b\.pdf)', htmlForParse)
+date = re.findall(r'\d\d\.\d\d\.\d{4}', htmlForParse)
+uid = re.findall(r'_blank">................', htmlForParse)
+print uid
+# \d{2}-/2018
+print pdfLinks
+print date
+#format {'uid': {'date': 'date string', 'url': 'link to pdf file'}}
+# logo = urllib.urlopen("http://doc.ksrf.ru/decision/KSRFDecision343971.pdf").read()
+# f = open("l.pdf", "wb")
+# f.write(logo)
+# f.close()
