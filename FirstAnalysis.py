ea22a58203da91d98cd94d4c49458c92c7a7c00a
Drokonor <terminusdrok@gmail.com>
2018-09-30 21:04:50 +0300

    First working version of rude analysis

--- FirstAnalysis.py
+++ FirstAnalysis.py
@@ -2,7 +2,7 @@ import re
 pattern = re.compile(r'[Оо].[\s\d]+[яфмаисонд]\w+[\s\d]+года[\s\d]+№[\s\d]+[-\w]+')
 
 def GetRudeLinks(filename):
-    file = open(filename,'r')
+    file = open(filename,'r', encoding="utf-8")
     R = file.read()
     file.close()
     Slice = re.split(r'[Мм]нение\s[Сс]удьи\s[Кк]онст',R,maxsplit=1)
@@ -16,5 +16,5 @@ def GetRudeLinksForMultipleDocuments(dictoftexts):
     return dictofrudelinks
 
 if (__name__ == '__main__'):
-    dictoftexts = {'35-П/2018':{'date':'str','url': r'C:\Users\GameOS\Desktop\Grub\input.txt','title':'Full title'}}
+    dictoftexts = {'35-П/2018':{'date':'str','url': r'C:\Users\GameOS\Desktop\Grub\input1.txt','title':'Full title'}}
     print (GetRudeLinksForMultipleDocuments(dictoftexts))
