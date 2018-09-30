28c3edec02c9372f8ca0187184b4344fa276be47
Drokonor <terminusdrok@gmail.com>
2018-09-30 14:12:22 +0300

    version 1.0 for RudeLinks

--- FirstAnalysis.py
+++ FirstAnalysis.py
@@ -1,19 +1,22 @@
 import re
-import os 
-files = os.listdir()
-texts = filter(lambda x: x.endswith('.txt'), files)
-def FirstAnalysis(str,ALL):
- file = open(str,'r')
- R = file.read()
- file.close()
- n = R.find('Мнение судьи Конст')
- R = R[0:n]
 
- pattern = re.compile(r'[Оо].\s\d+\s[яфмаисонд]\w+\s\d+\sгода\s№\s\d[^ ),]+')
- result = pattern.findall(R)
- #print (result)
- ALL[str] = result
-ALL = {} 
-for text in texts:
- FirstAnalysis(text,ALL) 
-print (ALL)
+def GetRudeLinks(filename):
+    file = open(filename,'r')
+    R = file.read()
+    file.close()
+    n = R.find('Мнение судьи Конст')
+    R = R[0:n] #to do:optimize
+
+    pattern = re.compile(r'[Оо].[ \d]+[яфмаисонд]\w+[ \d]+года[ \d]+№[ \d]+[^ ),]+')
+    result = pattern.findall(R)
+    return result    
+
+def GetRudeLinksForMultipleDocuments(dictoftexts):
+    dictofrudelinks = {}
+    for key in sorted(dictoftexts):
+        dictofrudelinks[key] = GetRudeLinks(dictoftexts[key]['url'])
+    return dictofrudelinks
+
+if (__name__ == '__main__'):
+    dictoftexts = {'35-П/2018':{'date':'str','url': r'C:\Users\GameOS\Desktop\Grub\input.txt','title':'Full title'}}
+    print (GetRudeLinksForMultipleDocuments(dictoftexts))
