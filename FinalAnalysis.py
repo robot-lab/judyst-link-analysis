42cdd7915df8a52bfd8cb5febe12b59592321197
Oleg Navolotsky <oleg.navolotsky@gmail.com>
2018-09-30 22:49:00 +0300

    changed code style to pep8 for whole code (checked in vs code with python.linting.pep8Enabled option)

--- FinalAnalysis.py
+++ FinalAnalysis.py
@@ -1,32 +1,8 @@
-if __name__=='__main__':
-    courtSiteContent = {}
-    collectedLinks = {}
-    courtSiteContent['1-П/2002'] = {'date': '15.01.2002', 'title': 'по делу о проверке конституционности отдельных положений статьи 64 Федерального закона "Об основных гарантиях избирательных прав и права на участие в референдуме граждан Российской Федерации" и статьи 92 Федерального закона "О выборах депутатов Государственной Думы Федерального Собрания Российской Федерации" в связи с жалобой гражданина А.М.Траспова', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30284.pdf'}
-    courtSiteContent['8-П/2003'] = {'date': '14.05.2003', 'title': 'по делу о проверке конституционности пункта 2 статьи 14 Федерального закона "О судебных приставах" в связи с запросом Лангепасского городского суда Ханты-Мансийского автономного округа', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30345.pdf'}
-    courtSiteContent['8-П/2005'] = {'date': '14.07.2005', 'title': 'по делу о проверке конституционности отдельных положений федеральных законов о федеральном бюджете на 2003 год, на 2004 год и на 2005 год и постановления Правительства Российской Федерации "О порядке исполнения Министерством финансов Российской Федерации судебных актов по искам к казне Российской Федерации на возмещение вреда, причиненного незаконными действиями (бездействием) органов государственной власти либо должностных лиц органов государственной власти" в связи с жалобами граждан Э.Д.Жуховицкого, И.Г.Пойма, А.В.Понятовского, А.Е.Чеславского и ОАО "Хабаровскэнерго"', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30352.pdf'}
-    courtSiteContent['10-П/2007'] = {'date': '12.07.2007', 'title': 'по делу о проверке конституционности положения абзаца третьего части первой статьи 446 Гражданского процессуального кодекса Российской Федерации в связи с жалобами граждан В.В.Безменова и Н.В.Калабуна  ', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision19712.pdf'}
-    courtSiteContent['4-П/2010'] = {'date': '26.02.2010', 'title': 'по делу о проверке конституционности части второй статьи 392 Гражданского процессуального кодекса Российской Федерации в связи с жалобами граждан А.А.Дорошка, А.Е.Кота и Е.Ю.Федотовой', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision36802.pdf'}
-    courtSiteContent['11-П/2012'] = {'date': '14.05.2012', 'title': 'по делу о проверке конституционности положения абзаца второго части первой статьи 446 Гражданского процессуального кодекса Российской Федерации в связи с жалобами граждан Ф.Х.Гумеровой и Ю.А.Шикунова  ', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision99813.pdf'}
-    courtSiteContent['7-П/2016'] = {'date': '10.03.2016', 'title': 'по делу о проверке конституционности части 1 статьи 21, части 2 статьи 22 и части 4 статьи 46 Федерального закона «Об исполнительном производстве» в связи с жалобой гражданина М.Л.Ростовцева', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision225558.pdf'}
-    courtSiteContent['1-П/2001'] = {'date': '25.01.2001', 'title': 'по делу о проверке конституционности положения пункта 2 статьи 1070 Гражданского кодекса Российской Федерации в связи с жалобами граждан И.В.Богданова, А.Б.Зернова, С.И.Кальянова и Н.В.Труханова', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30415.pdf'}
-    courtSiteContent['7-П/1995'] = {'date': '06.06.1995', 'title': 'по делу о проверке конституционности абзаца 2 части седьмой статьи 19 Закона РСФСР от 18 апреля 1991 года "О милиции" в связи с жалобой гражданина В.М.Минакова', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30417.pdf'}
-    courtSiteContent['14-П/1996'] = {'date': '13.06.1996', 'title': 'по делу о проверке конституционности части пятой статьи 97 Уголовно-процессуального кодекса РСФСР в связи с жалобой гражданина В.В.Щелухина', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30276.pdf'}
-    courtSiteContent['14-П/1999'] = {'date': '28.10.1999', 'title': 'по делу о проверке конституционности статьи 2 Федерального закона "О внесении изменений и дополнений в Закон Российской Федерации "О налоге на прибыль предприятий и организаций" в связи с жалобой ОАО "Энергомашбанк"', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30307.pdf'}
-    courtSiteContent['14-П/2000'] = {'date': '22.11.2000', 'title': 'по делу о проверке конституционности части третьей статьи 5 Федерального закона "О государственной поддержке средств массовой информации и книгоиздания Российской Федерации"', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30294.pdf'}
-    courtSiteContent['12-П/2003'] = {'date': '14.07.2003', 'title': 'по делу о проверке конституционности положений статьи 4, пункта 1 статьи 164, пунктов 1 и 4 статьи 165 Налогового кодекса Российской Федерации, статьи 11 Таможенного кодекса Российской Федерации и статьи 10 Закона Российской Федерации "О налоге на добавленную стоимость" в связи с запросами Арбитражного суда Липецкой области, жалобами ООО "Папирус", ОАО "Дальневосточное морское пароходство" и ООО "Коммерческая компания "Балис"', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30381.pdf'}
-    courtSiteContent['6-П/2015'] = {'date': '31.03.2015', 'title': 'по делу о проверке конституционности пункта 1 части 4 статьи 2 Федерального конституционного закона «О Верховном Суде Российской Федерации» и абзаца третьего подпункта 1 пункта 1 статьи 342 Налогового кодекса Российской Федерации в связи с жалобой открытого акционерного общества «Газпром нефть»', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision191663.pdf'}
-    courtSiteContent['11-П/2002'] = {'date': '19.06.2002', 'title': 'по делу о проверке конституционности ряда положений Закона Российской Федерации от 18 июня 1992 года "О социальной защите граждан, подвергшихся воздействию радиации вследствие катастрофы на Чернобыльской АЭС" (в редакциях от 24 ноября 1995 года и от 12 февраля 2001 года), Федеральных законов от 12 февраля 2001 года "О внесении изменений и дополнений в Закон Российской Федерации "О социальной защите граждан, подвергшихся воздействию радиации вследствие катастрофы на Чернобыльской АЭС", от 19 июня 2000 года "О минимальном размере оплаты труда" и от 7 августа 2000 года "О порядке установления размеров стипендий и социальных выплат в Российской Федерации" в связи с запросами Верховного Суда Российской Федерации и Октябрьского районного суда города Краснодара, жалобами граждан и общественных организаций чернобыльцев', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision30304.pdf'}
-    courtSiteContent['244-О-П/2008'] = {'date': '20.03.2008', 'title': 'по жалобе гражданина Петрова Игоря Александровича на нарушение его конституционных прав частью первой статьи 208 Гражданского процессуального кодекса Российской Федерации', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision17279.pdf'}
-    courtSiteContent['738-О-О/2008'] = {'date': '06.10.2008', 'title': 'об отказе в принятии к рассмотрению жалобы общества с ограниченной ответственностью «Дальневосточная трастовая компания» на нарушение конституционных прав и свобод частью 1 статьи 183 Арбитражного процессуального кодекса Российской Федерации', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision17779.pdf'}
-    courtSiteContent['1469-О/2015'] = {'date': '23.06.2015', 'title': 'об отказе в принятии к рассмотрению жалобы гражданина Кошелева Вячеслава Петровича на нарушение его конституционных прав частью первой статьи 208 Гражданского процессуального кодекса Российской Федерации', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision203108.pdf'}
-    courtSiteContent['364-О/2005'] = {'date': '04.10.2005', 'title': 'по запросу Верховного Суда Российской Федерации о проверке конституционности ряда положений Федерального закона "О внесении изменений в статью 5 Закона Российской Федерации "О социальной защите граждан, подвергшихся воздействию радиации вследствие катастрофы на Чернобыльской АЭС" и в статью 2 Федерального закона "О внесении изменений и дополнений в Закон Российской Федерации "О социальной защите граждан, подвергшихся воздействию радиации вследствие катастрофы на Чернобыльской АЭС"', 'link': 'http://doc.ksrf.ru/decision/KSRFDecision31253.pdf'}
-
-    collectedLinks['35-П/2018'] = ['от 15 января 2002 года № 1-П', 'от 14 мая 2003 6 года №8-П', 'от 14 июля 2005 года № 8-П', 'от 12 июля 2007 года № 10-П', 'от 26 февраля 2010 года № 4-П', 'от 14 мая 2012 года № 11-П', 'от 10 марта 2016 года № 7-П', 'от 25 января 2001 года № 1-П', 'от 6 июня 1995 года № 7-П', 'от 13 июня 1996 года № 14-П', 'от 28 октября 1999 года № 14-П', 'от 22 ноября 2000 года № 14-П', 'от 14 июля 2003 года № 12-П', 'от 12 июля 2007 года № 10-П', 'от 31 марта 2015 года № 6-П', 'от 19 июня 2002 года № 11-П', 'от 20 марта 2008 года № 244-О-П', 'от 6 октября 2008 года № 738-О-О', 'от 23 июня 2015 года № 1469-О', 'от 23 июня 2015 года № 1469-О', 'от 4 октября 2005 года № 364-О']
-
 import re
 
 yearPattern = re.compile(r'(?<=\s)\d{4}(?=\s)')
 numberPattern = re.compile(r'\d+(-[А-Яа-я]+)+')
+
 
 def GetCleanLinks(collectedLinks, courtSiteContent):
     '''
@@ -34,8 +10,8 @@ def GetCleanLinks(collectedLinks, courtSiteContent):
     arguments:
     collectedLinks: a dictionary with dirty links list as element and
     string with court decision ID (uid) as a key.
-    courtSiteContent: a dictionary with dictionary such as 
-    {'date': 'date_str', 'title': 'title_str', 'link': 'link_str'} 
+    courtSiteContent: a dictionary with dictionary such as
+    {'date': 'date_str', 'title': 'бла-бла', 'link': 'link_str'}
     as element and string with court decision ID (uid) as a key.
     '''
     rejectedLinks = {}
@@ -43,30 +19,172 @@ def GetCleanLinks(collectedLinks, courtSiteContent):
     for courtDecisionID in sorted(collectedLinks):
         for link in collectedLinks[courtDecisionID]:
             spam = re.split(r'(№|N)', link)
-            number =  numberPattern.search(spam[-1])
-            years =  yearPattern.findall(spam[0])
+            number = numberPattern.search(spam[-1])
+            years = yearPattern.findall(spam[0])
             if years and number:
                 eggs = False
                 while years:
                     gottenID = number[0] + '/' + years.pop()
                     if gottenID in courtSiteContent:
-                        if not courtDecisionID in checkedLinks:
+                        if courtDecisionID not in checkedLinks:
                             checkedLinks[courtDecisionID] = []
                         if gottenID not in checkedLinks[courtDecisionID]:
                             checkedLinks[courtDecisionID].append(gottenID)
                         eggs = True
                         years.clear()
                 if not eggs:
-                    if not courtDecisionID in rejectedLinks:
+                    if courtDecisionID not in rejectedLinks:
                         rejectedLinks[courtDecisionID] = []
                     rejectedLinks[courtDecisionID].append(link)
             else:
-                if not courtDecisionID in rejectedLinks:
+                if courtDecisionID not in rejectedLinks:
                     rejectedLinks[courtDecisionID] = []
                 rejectedLinks[courtDecisionID].append(link)
     return (checkedLinks, rejectedLinks)
 
-if __name__=='__main__':
+
+def GetLinkGraph(checkedLinks):
+    '''
+    Gets Link Graph, returning tuple (vertices = [],
+    edgeList = [(citingDecisionID, citedDecisionID), (),...]).
+    argument: checkedLinks is a dictionary with clean links list
+    as element and string with court decision ID (uid) as a key.
+    '''
+    vertices = list(set(sorted(checkedLinks) +
+                        [link for citingDecisionID in sorted(checkedLinks)
+                         for link in checkedLinks[citingDecisionID]]))
+    vertices.sort()
+    edgeList = []
+    for citingDecisionID in vertices:
+        if citingDecisionID in checkedLinks:
+            for citedDecisionID in vertices:
+                if citedDecisionID in checkedLinks[citingDecisionID]:
+                    edgeList.append((citingDecisionID, citedDecisionID))
+    return (vertices, edgeList)
+
+if __name__ == '__main__':
+    courtSiteContent = {}
+    collectedLinks = {}
+    courtSiteContent['1-П/2002'] = {
+        'date': '15.01.2002',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30284.pdf'
+        }
+    courtSiteContent['8-П/2003'] = {
+        'date': '14.05.2003',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30345.pdf'
+        }
+    courtSiteContent['8-П/2005'] = {
+        'date': '14.07.2005',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30352.pdf'
+        }
+    courtSiteContent['10-П/2007'] = {
+        'date': '12.07.2007',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision19712.pdf'
+        }
+    courtSiteContent['4-П/2010'] = {
+        'date': '26.02.2010',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision36802.pdf'
+        }
+    courtSiteContent['11-П/2012'] = {
+        'date': '14.05.2012',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision99813.pdf'
+        }
+    courtSiteContent['7-П/2016'] = {
+        'date': '10.03.2016',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision225558.pdf'
+        }
+    courtSiteContent['1-П/2001'] = {
+        'date': '25.01.2001',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30415.pdf'
+        }
+    courtSiteContent['7-П/1995'] = {
+        'date': '06.06.1995',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30417.pdf'
+        }
+    courtSiteContent['14-П/1996'] = {
+        'date': '13.06.1996',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30276.pdf'
+        }
+    courtSiteContent['14-П/1999'] = {
+        'date': '28.10.1999',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30307.pdf'
+        }
+    courtSiteContent['14-П/2000'] = {
+        'date': '22.11.2000',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30294.pdf'
+        }
+    courtSiteContent['12-П/2003'] = {
+        'date': '14.07.2003',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30381.pdf'
+        }
+    courtSiteContent['6-П/2015'] = {
+        'date': '31.03.2015',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision191663.pdf'
+        }
+    courtSiteContent['11-П/2002'] = {
+        'date': '19.06.2002',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision30304.pdf'
+        }
+    courtSiteContent['244-О-П/2008'] = {
+        'date': '20.03.2008',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision17279.pdf'
+        }
+    courtSiteContent['738-О-О/2008'] = {
+        'date': '06.10.2008',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision17779.pdf'
+        }
+    courtSiteContent['1469-О/2015'] = {
+        'date': '23.06.2015',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision203108.pdf'
+        }
+    courtSiteContent['364-О/2005'] = {
+        'date': '04.10.2005',
+        'title': 'бла-бла',
+        'link': 'http://doc.ksrf.ru/decision/KSRFDecision31253.pdf'
+        }
+
+    collectedLinks['35-П/2018'] = [
+        'от 15 января 2002 года № 1-П',
+        'от 14 мая 2003 6 года №8-П',
+        'от 14 июля 2005 года № 8-П',
+        'от 12 июля 2007 года № 10-П',
+        'от 26 февраля 2010 года № 4-П',
+        'от 14 мая 2012 года № 11-П',
+        'от 10 марта 2016 года № 7-П',
+        'от 25 января 2001 года № 1-П',
+        'от 6 июня 1995 года № 7-П',
+        'от 13 июня 1996 года № 14-П',
+        'от 28 октября 1999 года № 14-П',
+        'от 22 ноября 2000 года № 14-П',
+        'от 14 июля 2003 года № 12-П',
+        'от 12 июля 2007 года № 10-П',
+        'от 31 марта 2015 года № 6-П',
+        'от 19 июня 2002 года № 11-П',
+        'от 20 марта 2008 года № 244-О-П',
+        'от 6 октября 2008 года № 738-О-О',
+        'от 23 июня 2015 года № 1469-О',
+        'от 23 июня 2015 года № 1469-О',
+        'от 4 октября 2005 года № 364-О'
+        ]
+
     response = GetCleanLinks(collectedLinks, courtSiteContent)
     checkedLinks = response[0]
     rejectedLinks = response[1]
@@ -75,27 +193,6 @@ if __name__=='__main__':
     print("Rejected links: ")
     print(rejectedLinks)
 
-def GetLinkGraph(checkedLinks):
-    '''
-    Gets Link Graph, returning tuple (vertices = [], 
-    edgeList = [(citingDecisionID, citedDecisionID), (),...]).
-    argument: checkedLinks is a dictionary with clean links list
-    as element and string with court decision ID (uid) as a key.
-    '''
-    vertices = list(set(sorted(checkedLinks) +
-                        [link for citingDecisionID in sorted(checkedLinks)
-                         for link in checkedLinks[citingDecisionID]]))
-                    
-    vertices.sort()
-    edgeList = []
-    for citingDecisionID in vertices:
-        if citingDecisionID in checkedLinks:
-            for citedDecisionID in vertices:
-                if citedDecisionID in checkedLinks[citingDecisionID]:
-                     edgeList.append((citingDecisionID, citedDecisionID))
-    return (vertices, edgeList)
-
-if __name__=='__main__':
     reponse2 = GetLinkGraph(checkedLinks)
     vertices = reponse2[0]
     edgeList = reponse2[1]
--- law.py
+++ law.py
@@ -1,40 +1,44 @@
 import os.path
 import urllib.request
 import pdfminer.high_level  # python -m pip install pdfminer.six
-import lxml.html as html # python -m pip install lxml
+import lxml.html as html  # python -m pip install lxml
+
 
 def GetResolutionHeaders():
     page = html.parse('http://www.ksrf.ru/ru/Decision/Pages/default.aspx')
     decisions = page.getroot().find_class('ms-alternating') + \
-                    page.getroot().find_class('ms-vb') 
-    court_site_content = {} 
-    for d in decisions: 
-        decision_id = d[2].text_content() 
-        court_site_content[decision_id] = {} 
-        court_site_content[decision_id]['date'] = d[0].text_content() 
-        court_site_content[decision_id]['title'] = d[1].text_content() 
+        page.getroot().find_class('ms-vb')
+    court_site_content = {}
+    for d in decisions:
+        decision_id = d[2].text_content()
+        court_site_content[decision_id] = {}
+        court_site_content[decision_id]['date'] = d[0].text_content()
+        court_site_content[decision_id]['title'] = d[1].text_content()
         court_site_content[decision_id]['link'] = \
             d[2].getchildren()[0].get('href')
     return court_site_content
 
-def LoadResolutionTexts(court_site_content, folderName = 'Decision Files'):
+
+def LoadResolutionTexts(court_site_content, folderName='Decision Files'):
     if not os.path.exists(folderName):
         os.mkdir(folderName)
     for decision_id in sorted(court_site_content):
         logo = urllib.request.urlopen(
                 court_site_content[decision_id]['link']).read()
-        path_to_pdf = os.path.join(folderName, decision_id.replace('/','_') + '.pdf')
-        path_to_txt = os.path.join(folderName, decision_id.replace('/','_') + '.txt')
+        path_to_pdf = os.path.join(folderName,
+                                   decision_id.replace('/', '_') + '.pdf')
+        path_to_txt = os.path.join(folderName,
+                                   decision_id.replace('/', '_') + '.txt')
         with open(path_to_pdf, "wb") as pdf_file:
-             pdf_file.write(logo)
+            pdf_file.write(logo)
         with open(path_to_pdf, "rb") as pdf_file, \
                 open(path_to_txt, 'wb') as txt_file:
             pdfminer.high_level.extract_text_to_fp(pdf_file, txt_file)
         court_site_content[decision_id]['link'] = path_to_txt
         os.remove(path_to_pdf)
     return court_site_content
-    
-if __name__=='__main__':
+
+if __name__ == '__main__':
     court_site_content = GetResolutionHeaders()
-    court_site_content= LoadResolutionTexts(court_site_content)
+    court_site_content = LoadResolutionTexts(court_site_content)
     print(court_site_content)
