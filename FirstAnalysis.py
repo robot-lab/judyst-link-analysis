b487ba59bccebee6130a1f3ca9676b111cd15a2b
Drokonor <terminusdrok@gmail.com>
2018-10-02 15:33:24 +0300

    first working version of rude analysis

--- FirstAnalysis.py
+++ FirstAnalysis.py
@@ -1,13 +1,16 @@
 import re
-pattern = re.compile(r'[Оо].[\s\d]+[яфмаисонд]\w+[\s\d]+года[\s\d]+№[\s\d]+[-\w]+')
+pattern = re.compile(
+    r'[Оо].[\s\d]+[яфмаисонд]\w+[\s\d]+года[\s\d]+№[\s\d]+[-\w]+')
+
 
 def GetRudeLinks(filename):
-    file = open(filename,'r', encoding="utf-8")
+    file = open(filename, 'r', encoding="utf-8")
     R = file.read()
     file.close()
-    Slice = re.split(r'[Мм]нение\s[Сс]удьи\s[Кк]онст',R,maxsplit=1)
+    Slice = re.split(r'[Мм]нение\s[Сс]удьи\s[Кк]онст', R, maxsplit=1)
     result = pattern.findall(Slice[0])
-    return result    
+    return result
+
 
 def GetRudeLinksForMultipleDocuments(dictoftexts):
     dictofrudelinks = {}
@@ -15,6 +18,12 @@ def GetRudeLinksForMultipleDocuments(dictoftexts):
         dictofrudelinks[key] = GetRudeLinks(dictoftexts[key]['url'])
     return dictofrudelinks
 
+
 if (__name__ == '__main__'):
-    dictoftexts = {'35-П/2018':{'date':'str','url': r'C:\Users\GameOS\Desktop\Grub\input1.txt','title':'Full title'}}
-    print (GetRudeLinksForMultipleDocuments(dictoftexts))
+    dictoftexts = {
+        '35-П/2018': {
+            'date': 'str',
+            'url': r'C:\Users\GameOS\Desktop\Grub\input1.txt',
+            'title': 'Full title'}
+            }
+    print(GetRudeLinksForMultipleDocuments(dictoftexts))
