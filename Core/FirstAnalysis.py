7d020984134491ef13395e554ef5ae640e1cff1e
Oleg Navolotsky <oleg.navolotsky@gmail.com>
2018-10-07 17:29:58 +0300

    added saving of duplicate headers

--- Core/FirstAnalysis.py
+++ Core/FirstAnalysis.py
@@ -1,32 +1,37 @@
 import re
 import sys
+
 pattern = re.compile(
     r'[Оо].[\s\d]+[яфмаисонд]\w+[\s\d]+года[\s\d]+№[\s\d]+[-\w]+')
 
 
-def GetRudeLinks(filename, key):
-    file = open(filename, 'r', encoding="utf-8")
+def GetRudeLinks(pathToTextFile, decisionID):
+    file = open(pathToTextFile, 'r', encoding="utf-8")
     text = file.read()
     file.close()
-    Slice = re.split(r'/', key)
-    key = Slice[0]
-    Opinion = re.search('№ ' + key, text)
-    endpos = Opinion.start() if Opinion != None else sys.maxsize
-    result = pattern.findall(text, endpos= endpos)
+    docNumber = re.split(r'/', decisionID)[0]
+    opinion = re.search('№ ' + docNumber, text)
+    endpos = opinion.start() if opinion else sys.maxsize
+    result = pattern.findall(text, endpos=endpos)
     return result
 
-def GetRudeLinksForMultipleDocuments(dictoftexts):
-    dictofrudelinks = {}
-    for key in dictoftexts:
-        dictofrudelinks[key] = GetRudeLinks(dictoftexts[key]['url'], key)
-    return dictofrudelinks
+
+def GetRudeLinksForMultipleDocuments(headers):
+    rudeLinks = {}
+    for decisionID in headers:
+        if 'not unique' in headers[decisionID]:
+            continue
+        rudeLinks[decisionID] = GetRudeLinks(
+            headers[decisionID]['path to text file'], decisionID)
+    return rudeLinks
 
 
 if (__name__ == '__main__'):
     dictoftexts = {
         '35-П/2018': {
             'date': 'str',
-            'url': r'C:\Users\GameOS\Desktop\Grub\input1.txt',
+            'url': 'some_url',
+            'path to text file': r'C:\Users\GameOS\Desktop\Grub\input1.txt',
             'title': 'Full title'}
             }
     print(GetRudeLinksForMultipleDocuments(dictoftexts))
--- Core/law.py
+++ Core/law.py
@@ -1,32 +1,33 @@
 import os.path
+
 import urllib.request
-import pdfminer.high_level  # python -m pip install pdfminer.six
-import lxml.html as html  # python -m pip install lxml
+# License: MIT License
+
+import pdfminer.high_level
+# License: MIT License; Installing: python -m pip install pdfminer.six
+
+import lxml.html as html
+# License: BSD license; Installing: python -m pip install lxml
+
 from selenium import webdriver
-  
+# License: Apache License 2.0
+
 
 def GetWebDriver(pathToChromeWebDriver='Core\\Selenium\\chromedriver.exe',
-         pageUri = "http://www.ksrf.ru/ru/Decision/Pages/default.aspx"):
+                 pageUri="http://www.ksrf.ru/ru/Decision/Pages/default.aspx"):
     driver = webdriver.Chrome(pathToChromeWebDriver)
     driver.get(pageUri)
     return driver
 
 
-
 def GetOpenPageScriptTemplate(driver, templateKey='NUMBER'):
     page = html.document_fromstring(driver.page_source)
-    td_list = page.find_class('UserSectionFooter ms-WPBody srch-WPBody')[0]. \
-        getchildren()[0]. \
-        getchildren()[0]. \
-        getchildren()[0]. \
-        getchildren()[0]. \
-        getchildren()
-    
-    template = td_list[-1].getchildren()[0].get('href')
+    template = page.find_class('UserSectionFooter ms-WPBody srch-WPBody')[0]. \
+        find('td/table/tbody/tr/td/a').get('href')
     template = template.split(':')[1]
     template = template.split(',')[0] + ',\'Page$' + templateKey + '\');'
-    
     return template
+
 
 def GetOpenPageScript(scriptTemplate, pageNum, templateKey='NUMBER'):
     return scriptTemplate.replace(templateKey, str(pageNum))
@@ -38,71 +39,78 @@ def GetPageHtmlByNum(driver, openPagetScriptTemplate, pageNum):
     return driver.page_source
 
 
-
-
-
-  
-def GetResolutionHeaders(countOfPage = 100):
-    court_site_content = {}
+def GetResolutionHeaders(countOfPage=100):
+    # TO DO: check for that page is refreshed check for that
+    # page is refreshed (i have an idea)
+    courtSiteContent = {}
     driver = GetWebDriver()
     template = GetOpenPageScriptTemplate(driver)
     page = html.document_fromstring(driver.page_source)
-
-    #repeated_uids = []
-    for i in range(2,countOfPage + 2):
+    for i in range(2, countOfPage + 2):
         decisions = page.find_class('ms-alternating') + \
                 page.find_class('ms-vb')
         for d in decisions:
-            decision_id = d[2].text_content()
-            #if (decision_id in court_site_content):
-                #if (not decision_id in repeated_uids):
-                    #repeated_uids.append(decision_id)
-
-            court_site_content[decision_id] = {}
-            court_site_content[decision_id]['date'] = d[0].text_content()
-            court_site_content[decision_id]['title'] = d[1].text_content()
-            court_site_content[decision_id]['url'] = \
-                d[2].getchildren()[0].get('href')
+            decisionID = d[2].text_content().upper()
+            date = d[0].text_content()
+            title = d[1].text_content()
+            url = d[2].getchildren()[0].get('href')
+            headerElements = {'date': date, 'url': url, 'title': title}
+            if decisionID not in courtSiteContent:
+                courtSiteContent[decisionID] = headerElements
+            else:
+                if 'not unique' in courtSiteContent[decisionID]:
+                    eggs = False
+                    for header in courtSiteContent[decisionID][1]:
+                        if header['url'] == url:
+                            eggs = True
+                            break
+                    if not eggs:
+                        courtSiteContent[decisionID][1].append(headerElements)
+                else:
+                    notUniqueHeaders = \
+                        [courtSiteContent[decisionID], headerElements]
+                    courtSiteContent[decisionID] = \
+                        ('not unique', notUniqueHeaders)
         page = html.document_fromstring(GetPageHtmlByNum(driver, template, i))
-    
-    return court_site_content
-    #return court_site_content, repeated_uids
 
-
+    return courtSiteContent
 
 
 def GetdecisionFileNameByUid(uid, foldername, ext='txt'):
     return os.path.join(foldername, uid.replace('/', '_') + '.' + ext)
 
 
-def LoadResolutionTexts(court_site_content, folderName='Decision files'):
+def LoadResolutionTexts(courtSiteContent, folderName='Decision files'):
     if not os.path.exists(folderName):
         os.mkdir(folderName)
-    for decision_id in court_site_content:
+    for decisionID in courtSiteContent:
+        if 'not unique' in courtSiteContent[decisionID]:
+            continue
         logo = urllib.request.urlopen(
-                court_site_content[decision_id]['url']).read()
-        path_to_pdf = GetdecisionFileNameByUid(decision_id, folderName, 'pdf')
-        path_to_txt = GetdecisionFileNameByUid(decision_id, folderName, 'txt')
-        with open(path_to_pdf, "wb") as pdf_file:
-            pdf_file.write(logo)
-        with open(path_to_pdf, "rb") as pdf_file, \
-                open(path_to_txt, 'wb') as txt_file:
-            pdfminer.high_level.extract_text_to_fp(pdf_file, txt_file)
-        court_site_content[decision_id]['url'] = path_to_txt
-        os.remove(path_to_pdf)
-    return court_site_content
-
-
+                courtSiteContent[decisionID]['url']).read()
+        pathToPDF = GetdecisionFileNameByUid(decisionID, folderName, 'pdf')
+        pathToTXT = GetdecisionFileNameByUid(decisionID, folderName, 'txt')
+        with open(pathToPDF, "wb") as PDFFIle:
+            PDFFIle.write(logo)
+        with open(pathToPDF, "rb") as PDFFIle, \
+                open(pathToTXT, 'wb') as TXTFile:
+            pdfminer.high_level.extract_text_to_fp(PDFFIle, TXTFile)
+        courtSiteContent[decisionID]['path to text file'] = pathToTXT
+        os.remove(pathToPDF)
+    return courtSiteContent
 
 
 if __name__ == '__main__':
     import json
-    #court_site_content = GetResolutionHeaders()
-    #court_site_content = LoadResolutionTexts(court_site_content)
-    #print(court_site_content)
-    #driver = GetWebDriver()
-    #template = GetOpenPageScriptTemplate()
-    res, repeated = GetResolutionHeaders(1570)
-    file = open('repeated.json', 'w')
-    file.write(json.dumps(repeated))
-    file.close()
+    # court_site_content = GetResolutionHeaders()
+    # court_site_content = LoadResolutionTexts(court_site_content)
+    # print(court_site_content)
+    # driver = GetWebDriver()
+    # template = GetOpenPageScriptTemplate()
+
+    # res, repeated = GetResolutionHeaders(1570)
+    # file = open('repeated.json', 'w')
+    # file.write(json.dumps(repeated))
+    # file.close()
+
+    GetResolutionHeaders(666)
